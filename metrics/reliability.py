4f3e29ea332b22fc0f6fd45126963e08638009be
Andrea Campagner <a.campagner@campus.unimib.it>
2020-09-07 10:58:52 +0200

    Updated metrics and added sample data

--- metrics/reliability.py
+++ metrics/reliability.py
@@ -1,31 +1,65 @@
 import numpy as np
 
-def irr(preds, confs = None, cohers = None, accs = None):
-    res = 0
-    if confs is None:
-        confs = np.ones(preds.shape)*0.5
-    if cohers is None:
-        cohers = np.ones(preds.shape[1])
-    if accs is None:
-        accs = np.ones(preds.shape[1])
-    for row in range(len(preds)):
-        count = 0
-        r = preds[row]
-        den = 0
-        for i in range(len(r)):
-            for j in range(len(r)):
-                if i != j:
-                    den += 1
-                    if r[i] == r[j]:
-                        countn = confs[row,i]*confs[row,j]
-                        countd = confs[row,i]*confs[row,j]
-                        #countd = 0
-                        countd += confs[row,i]*(1 - confs[row,j])/2
-                        countd += confs[row,j]*(1 - confs[row,i])/2
-                        countd += (1- confs[row,i])*(1 - confs[row,j])/4
-                        count += countn/countd
-						count *= cohers[i]*cohers[j]*accs[i]*accs[j]
-                        #count += countn
-        count /= den
-        res += count
-    return res/preds.shape[0]
+def irr(preds, confs = None, accs = None, cohers=None):
+	res_min = 0
+	res_max = 0
+	if confs is None:
+		confs = np.ones(preds.shape)*0.5
+	if cohers is None:
+		cohers = np.zeros(preds.shape[1])
+	if accs is None:
+		accs = np.ones(preds.shape[1])
+	for row in range(len(preds)):
+		count_min = 0
+		count_max = 0
+		r = preds[row]
+		den = 0
+		for i in range(len(r)):
+			for j in range(len(r)):
+				if i != j:
+					den += 1
+					if r[i] == r[j]:
+						count_min_temp = count_sigma(confs[row, i], confs[row, j],
+												cohers[i], cohers[j])
+						count_max_temp = count_sigma(confs[row, i], confs[row, j],
+												cohers[i], cohers[j], mode='max')
+						acc = accs[i]*accs[j]
+						acc /= (accs[i]*accs[j] + (1-accs[i])*(1-accs[j]))
+						count_min_temp *= acc
+						count_max_temp *= acc
+						count_min += count_min_temp
+						count_max += count_max_temp
+						#count += countn
+		count_min /= den
+		count_max /= den
+		res_min += count_min
+		res_max += count_max
+	return res_min/preds.shape[0], res_max/preds.shape[0]
+	
+def count_sigma(conf_i, conf_j, cohers_i, cohers_j, mode='min'):
+	count = 0
+	conf_corr_i = conf_i
+	conf_corr_j = conf_j
+	if mode == 'min':
+		conf_corr_i -= cohers_i
+		conf_corr_j -= cohers_j
+	else:
+		conf_corr_i += cohers_i
+		conf_corr_j += cohers_j
+		
+	if conf_corr_i < 0:
+		conf_corr_i = 0
+	if conf_corr_j < 0:
+		conf_corr_j = 0
+	if conf_corr_i > 1:
+		conf_corr_i = 1
+	if conf_corr_j > 1:
+		conf_corr_j = 1		
+		
+	countn = conf_corr_i*conf_corr_j
+	countd = conf_corr_i*conf_corr_j
+	countd += conf_corr_i*(1 - conf_corr_j)/2
+	countd += conf_corr_j*(1 - conf_corr_i)/2
+	countd += (1- conf_corr_i)*(1 - conf_corr_j)/4
+	count += countn/countd
+	return count
--- metrics/representativeness.py
+++ metrics/representativeness.py
@@ -1,26 +1,54 @@
+from scipy.spatial.distance import pdist
+from scipy.spatial.distance import cdist
+import numpy as np
+from sklearn.model_selection import train_test_split
+from scipy.optimize import linear_sum_assignment
+from scipy.stats import ks_2samp
+from sklearn.utils import resample
+from scipy.stats import entropy
+from scipy.spatial.distance import jensenshannon
+from statsmodels.distributions.empirical_distribution import ECDF
+from sklearn.metrics.pairwise import pairwise_kernels
 
-def representativeness(dataset, X_train, X_test)
-	"""Computes the degree to which X_test is representative of X_train, by comparing their distribution of distances"""
-	distrib_train = pdist(X_train).flatten()
+def bootstrap(stat_value, data, statistic, iters=100):
+    pvalue = 0
+    for i in range(iters):
+        train, test = train_test_split(data, test_size=0.33, random_state = i)
+        stat = statistic(train,test)
+        if  stat >= stat_value:
+            pvalue += 1
+    return pvalue/100
 
-	train_idx, test_idx = linear_sum_assignment(cdist(X_train, X_test))
-	X_new_train = X_train.copy()
-	X_new_train[train_idx,:] = X_test[test_idx,:]
-	distrib_new_train = pdist(X_new_train).flatten()
+def mmd(X_train, X_test, kernel='sigmoid'):
+    intra = np.sum(pairwise_kernels(X_train, X_train, metric=kernel))
+    intra += np.sum(pairwise_kernels(X_test, X_test, metric=kernel))
+    extra = np.sum(pairwise_kernels(X_train, X_test, metric=kernel))
+    return intra - 2*extra
 
-	best = np.max(np.abs(distrib_new_train - distrib_train))
+def phi(X_train, X_test, statistic, reshape=False):
+    """Computes the degree to which X_test is representative of X_train, by comparing their distribution of distances"""
+    distrib_train = pdist(X_train).flatten()
 
-	iters = 100
-	count = 0
-	for r in range(iters):
-		print(r)
-		train, test = train_test_split(dataset, test_size=0.33, random_state = r)
-		distrib_train_temp = pdist(train).flatten()
-		train_idx, test_idx = linear_sum_assignment(cdist(train, test))
-		X_new_train = train.copy()
-		X_new_train[train_idx,:] = test[test_idx,:]
-		distrib_new_train_temp = pdist(X_new_train).flatten()
-		
-		if best <= np.max(np.abs(distrib_new_train_temp - distrib_train_temp)):
-			count += 1
-	return count/iters
+    train_idx, test_idx = linear_sum_assignment(cdist(X_train, X_test))
+    X_new_train = X_train.copy()
+    X_new_train[train_idx,:] = X_test[test_idx,:]
+    distrib_new_train = pdist(X_new_train).flatten()
+    stat = 0
+    if reshape == True:
+        stat = statistic(distrib_train.reshape(1,-1)[0], distrib_new_train.reshape(1,-1)[0])
+    else:
+        stat = statistic(distrib_train.reshape(1,-1), distrib_new_train.reshape(1,-1))
+    #print(stat)
+    return stat
+
+def ks_2samp_supp(distrib_1, distrib_2):
+    return ks_2samp(distrib_1, distrib_2).statistic
+
+def phi_ks(X_train, X_test):
+    return phi(X_train, X_test, ks_2samp_supp, reshape=True)
+
+def phi_js(X_train, X_test):
+    return phi(X_train, X_test, jensenshannon, reshape=True)
+
+def phi_mmd(X_train, X_test):
+    return phi(X_train, X_test, mmd, reshape=False)
