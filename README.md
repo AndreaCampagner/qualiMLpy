d9c6f9306bbec97f34508c44e0d477c1831b87b3
Andrea Campagner <a.campagner@campus.unimib.it>
2021-05-07 14:33:11 +0200

    update metrics and changed file name

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # qualiMLpy
+This a repository collecting all metrics, algorithms and pieces of code related to data quality
+for Machine Learning, developed by me and others at the MUDI lab (https://www.mudilab.net/mudi/) of
+the DISCo dept. @ University of Milano-Bicocca.
+
+The dependencies for all the code are: scikit-learn, numpy, scipy.
+
+Some of our code has also been provisionally deployed in the form of web sandbox tools:
+
+- Degree of correspondence: https://reprdeg-test.herokuapp.com/
+- Degree of concordance: https://reliability-test.herokuapp.com/
+
+You can find more information on most of these metrics in various articles:
+
+* Cabitza, F., Campagner, A., & Sconfienza, L. M. (2020).
+As if sand were stone. New concepts and metrics to probe the ground on which to build trustable AI.
+BMC Medical Informatics and Decision Making, 20(1), 1-21.
+
+* Cabitza, F., Campagner, A., Albano, D., et al. (2020).
+The elephant in the machine: Proposing a new metric of data reliability and its application to a medical case to assess classification reliability.
+Applied Sciences, 10(11), 4014.
+
+* Campagner, A., Sconfienza, L., & Cabitza, F. (2020).
+H-accuracy, an alternative metric to assess classification models in medicine.
+Digital Personalized Health and Medicine; Studies in Health Technology and Informatics; IOS Press: Amsterdam, The Netherlands, 270.
+
--- metrics/correspondence.py
+++ metrics/correspondence.py
@@ -7,8 +7,44 @@ from scipy.stats import ks_2samp
 from sklearn.utils import resample
 from scipy.stats import entropy
 from scipy.spatial.distance import jensenshannon
-from statsmodels.distributions.empirical_distribution import ECDF
-from sklearn.metrics.pairwise import pairwise_kernels
+from sklearn.metrics import pairwise_distances, pairwise_kernels
+from sklearn.neighbors import NearestNeighbors
+
+
+def degree_correspondance(X_a, X_b, metric='euclidean', iters = 100, averaged=False):
+	similarities = []
+	if not averaged:
+		return psi_similarity_test(X_a, X_b, metric, iters)
+	else:
+		for ind in range(len(X_b)):
+			similarities.append(psi_similarity_test(X_a, [X_b[ind]], metric, iters))
+		return np.mean(similarities)
+
+def psi_similarity_test(X_a, X_b, metric='euclidean', iters=100):
+  stat = psi_similarity_helper(X_a, X_b, metric)
+  pval = 0.0
+  X_tot = np.concatenate((X_a, X_b), axis=0)
+  for i in range(iters):
+    X_a_temp, X_b_temp = train_test_split(X_tot, train_size=len(X_a)/len(X_tot), random_state = i)
+    stat_temp = psi_similarity_helper(X_a_temp, X_b_temp, metric)
+    if stat <= stat_temp:
+      pval += 1.0
+  return pval/iters
+
+def psi_similarity_helper(X_a, X_b, metric='euclidean'):
+  intra = np.sum(pairwise_distances(X_a, X_a, metric=metric))
+  nn = NearestNeighbors()
+  nn.fit(X_a)
+
+  stat = 0
+  for x in X_b:
+    idx = nn.kneighbors([x], 1, return_distance=False)
+    temp = intra - np.sum(pairwise_distances(X_a, X_a[idx][0], metric=metric))
+    temp += np.sum(pairwise_distances(X_a, [x], metric=metric))
+    stat += temp - intra
+  stat /= len(X_b)
+  return stat
+
 
 def bootstrap(stat_value, data, statistic, iters=100, size_split=0.5):
     pvalue = 0
